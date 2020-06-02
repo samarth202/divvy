575e609acab551879200296d153b27f8b2c64078
Sam K <skumar@esi-estech.com>
2020-06-02 16:07:57 -0500

    station query optimized

--- functions/stations.js
+++ functions/stations.js
@@ -2,14 +2,21 @@
 const fetch = require('node-fetch');
 const config = require('../config/config.json');
 
-module.exports = {
+let stations = [];
 
-  async getStation(id) {
+
+const stationFunctions = {
+
+  async fetchStations() {
     const response = await fetch(config.stationsUrl)
       .then((res) => res.json());
 
     const { data: { stations = [] } = {} } = response;
 
+    return stations;
+  },
+
+  async getStation(id) {
     const results = stations.filter((e) => e.station_id === id);
 
     return results[0] || null;
@@ -18,7 +25,7 @@ module.exports = {
   async getStations(ids = []) {
     const results = [];
     await Promise.all(ids.map(async (id) => {
-      const result = this.getStation(id);
+      const result = await this.getStation(id);
 
       if (result) {
         results.push(results);
@@ -28,3 +35,14 @@ module.exports = {
     return results;
   },
 };
+
+console.log('Fetching stations, Please wait...');
+stationFunctions.fetchStations().then((res) => {
+  stations = res;
+  console.log('Stations fetched, count:', stations.length);
+}).catch((err) => {
+  console.error('Error fetching stations', err);
+});
+
+
+module.exports = stationFunctions;
--- test/stations.js
+++ test/stations.js
@@ -2,11 +2,19 @@
 const assert = require('assert');
 const stationFunctions = require('../functions/stations');
 
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+
 describe('functions/stations', () => {
   it('should query the station via the api', async () => {
+    await sleep(1000);
     const result = await stationFunctions.getStation('56');
 
     assert(result.station_id === '56', 'station ids match');
     assert(result.name === 'Desplaines St & Kinzie St', 'station name match');
-  });
+  }).timeout(5000);
 });
